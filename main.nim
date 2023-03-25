db70319e4fc2438774da289ca6f2da9303e360f1
AssFucker13 <AssFucker13@cumshittler.lol>
2023-03-25 19:57:01 +0100

    inital commit

---
+++ main.nim
@@ -0,0 +1,30 @@
+import std/random
+import std/httpclient
+
+# init rng
+randomize()
+
+
+
+proc getFuzz(lenght: int, possibleFuzz: string): string =
+    result = ""
+    for i in 1 .. lenght:
+        result.add(sample(possibleFuzz))
+
+
+proc scrapeCatbox(fileEnding: string): string =
+    let
+        url: string = "https://files.catbox.moe/" & getFuzz(6, "abcdefghijklmnopqrstuvwxyz0123456789") & fileEnding
+        client: HttpClient = newHttpClient()
+
+    
+    if client.get(url).code != Http404:
+        return url
+
+
+
+# valid urls will be printed to stdout
+while true:
+    var response: string = scrapeCatbox(".png")
+    if response != "":
+        echo response
